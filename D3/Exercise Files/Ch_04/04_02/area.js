c3eceb328540be09fd0700413ba3888599b7915a
John Robin <johnrobinlugtu@gmail.com>
2018-06-27 10:15:13 -0700

    d3

---
+++ D3/Exercise Files/Ch_04/04_02/area.js
@@ -0,0 +1,26 @@
+
+const dataArray = [];
+const dataYears = [];
+const height = 200;
+const width = 200;
+
+for(let i = 0; i < 50; i++) {
+    const val = Math.floor(Math.random() * 100);
+    dataArray.push(val);
+    dataYears.push((2000 + i).toString());
+}
+
+console.log(dataYears);
+
+// generator
+const area = d3.area()
+                 .x(function(d, i){ return i * 20; })
+                 .y0(height)
+                 .y1(function(d){ return height - d; });
+
+const svg = d3.select("body")
+                .append("svg")
+                    .attr("height", "100%")
+                    .attr("width", "100%");
+
+svg.append("path").attr("d", area(dataArray));
--- D3/Exercise Files/Ch_04/04_02/interpolate.js
+++ D3/Exercise Files/Ch_04/04_02/interpolate.js
@@ -1,1 +1,32 @@
 
+const dataArray = [
+    {x: 5, y: 5},
+    {x: 10, y: 15},
+    {x: 15, y: 10},
+    {x: 20, y: 22},
+    {x: 25, y: 5}
+];
+
+const svg = d3.select("body")
+                .append("svg")
+                    .attr("height", "100%")
+                    .attr("width", "100%");
+
+const line = d3.line()
+                 .x(function(d, i){ return d.x*6; })
+                 .y(function(d, i){ return d.y*4; })
+                 .curve(d3.curveCardinal);
+
+const chartGroup = svg.append("g").attr("transform", "translate(0, 0)");
+
+chartGroup.append("path")
+     .attr("fill", "none")
+     .attr("stroke", "blue")
+     .attr("d", line(dataArray));
+
+chartGroup.selectAll("circle")
+        .data(dataArray)
+        .enter().append("circle")
+                    .attr("cx",function(d, i){ return d.x*6; })
+                    .attr("cy",function(d, i){ return d.y*4; })
+                    .attr("r","2");
